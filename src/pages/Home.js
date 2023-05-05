ec66f309e32e7cf76cff242245aee91bc572e650
ricar <ricardocisneros197@gmail.com>
2023-05-04 22:44:37 -0700

    fixed issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
-import ServicesSection from '../components/ServicesSection';
+
 import ProjectsSection from '../components/ProjectsSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import ContactBanner from '../components/ContactBanner';
@@ -12,7 +12,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <AboutSection />
-      <ServicesSection />
+      
       <ProjectsSection />
       <TestimonialsSection />
       <ContactBanner />
