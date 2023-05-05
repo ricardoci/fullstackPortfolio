010da5e63950ec7af97d7686c2bbcfb2354d6037
Ricardo Cisneros <ricardocisneros197@gmail.com>
2023-05-04 22:02:46 -0700

    a

--- fullstackportfolio/server.js
+++ fullstackportfolio/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+// Catch-all route to serve the index.html file
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Start the server
 const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`App is listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
