5af44141bda748ebeaff5f8428602ac219a524b4
ricar <ricardocisneros197@gmail.com>
2023-05-31 02:49:23 -0700

    added another route handler

--- server/server.js
+++ server/server.js
@@ -19,6 +19,13 @@ app.get('/FullStackResume.pdf', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Start the server
 const port = process.env.PORT || 3001;
