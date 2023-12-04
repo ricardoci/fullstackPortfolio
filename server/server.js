fdc84e43a7adb516e0af0e134cef6d84428c714e
ricar <ricardocisneros197@gmail.com>
2023-12-04 15:51:17 -0800

    a

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
