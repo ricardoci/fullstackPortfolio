20b26dfc22bee79f5f9e58d8bde308e0f3faf28c
ricar <ricardocisneros197@gmail.com>
2023-12-04 13:56:57 -0800

    a

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-import "./server/server"
+require('./server/server.js');
+const server = require('./server/server');
+
+// Now you can use 'server' here
+// For example, start your server if it's exported as a function:
+
+
+// Start the server
--- server/server.js
+++ server/server.js
@@ -25,3 +25,5 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
+
+module.exports = app;
